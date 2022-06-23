e55f219dc881a746cd897b882a960ecb64152d51
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-06-23 10:37:29 +0400

    12
    12

--- Palindrom/Palindrom/Program.cs
+++ Palindrom/Palindrom/Program.cs
@@ -9,30 +9,41 @@ namespace Palindrom
             //Задача №3
             string num = Console.ReadLine();
             string last = num[num.Length - 1].ToString();
+            string first = num[num.Length - (num.Length - 1)].ToString();
             bool bol = true;
-            if (num.Length > 11)
+            //uint nature;
+
+            if (UInt32.TryParse(num, out _))
             {
-                Console.WriteLine("Введите число не превышающее 10^9");
-                return;
-            }
-            if (last == "0")
-            {
-                Console.WriteLine("Последние число не должно быть равным нулю");
-                return;
-            }
-            if (!String.IsNullOrEmpty(num))
-            {
-                for (int i = 0; i < num.Length / 2; i++)
+
+                if (num.Length > 11)
                 {
-                    if (num[i] != num[num.Length - 1 - i])
-                    {
-                        bol = false;
-                        Console.WriteLine("Введенное число не является палиндромом");
-                        break;
-                    }
+                    Console.WriteLine("Введите натуральное число не превышающее 10^9");
+                    return;
                 }
-                if (bol == true)
-                    Console.WriteLine("палиндромом");
+                if (last == "0" && first == "0")
+                {
+                    Console.WriteLine("Число должно быть натуральным и последние число не должно быть равным нулю");
+                    return;
+                }
+                if (!String.IsNullOrEmpty(num))
+                {
+                    for (int i = 0; i < num.Length / 2; i++)
+                    {
+                        if (num[i] != num[num.Length - 1 - i])
+                        {
+                            bol = false;
+                            Console.WriteLine("Введенное число не является палиндромом");
+                            break;
+                        }
+                    }
+                    if (bol == true)
+                        Console.WriteLine("палиндромом");
+                }
+                else
+                {
+                    Console.WriteLine("Введенное число не является палиндромом");
+                }
             }
             else
             {
