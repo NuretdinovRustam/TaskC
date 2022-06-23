cbc81bd602e1dc3f0e7a28c04451d139faafa074
NuretdinovRustam <107863747+NuretdinovRustam@users.noreply.github.com>
2022-06-23 12:15:17 +0400

    Изменил логику в 4.
    Изменил логику в 4.

--- NumbersCount/NumbersCount/Program.cs
+++ NumbersCount/NumbersCount/Program.cs
@@ -9,10 +9,11 @@ namespace NumbersCount
             //Задача №4
             Console.WriteLine("Введите натурально число не превышающее <= 10^5");
             string num1 = Console.ReadLine();
+            //добавил try если начнут вводить некорректные символы
             try
             {
                 string first = num1[0].ToString();
-                int num2 = 1;
+                //int num2 = 1;
                 int num4 = 0;
                 if (num1.Length > 6 | first == "0" | !UInt32.TryParse(num1, out _))
                 {
@@ -21,17 +22,17 @@ namespace NumbersCount
                 }
                 for (int i = 0; i < Int32.Parse(num1); i++)
                 {
-                    string num3 = num2.ToString();
-                    string last = num3[num3.Length - 1].ToString();
-                    num2++;
+                    string num5 = Console.ReadLine();
+                    string last = num5[num5.Length - 1].ToString();
+                    if (Int32.Parse(num5) > 2 * (Math.Pow(10, 6)))
+                    {
+                        Console.WriteLine("Введено некорректное значение");
+                    }
                     if (last == "0")
                     {
                         num4++;
                     }
-                    if (num2 > 2 * (Math.Pow(10, 6)))
-                    {
-                        break;
-                    }
+
                 }
                 Console.WriteLine($"Количество нулей во всех числах: {num4}");
             }
@@ -39,7 +40,7 @@ namespace NumbersCount
             {
                 Console.WriteLine("Введено некорректное значение");
             }
-            // for(int i = 1; i < Math.Pow(10, num1.Length); i++)
+
 
         }
     }
